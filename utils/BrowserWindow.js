ed2a6acecdf737f71062dce2c8199c91ee4d34af
Klout <55015851+KloutDevs@users.noreply.github.com>
2020-12-21 17:40:35 -0300

    Updated BrowserWindow with a debug mode included.
    If debugMode is set to true, it will be notified in the console. If an error appears, it will be known in which process it occurred.

--- utils/BrowserWindow.js
+++ utils/BrowserWindow.js
@@ -4,33 +4,57 @@ const chalk = require('chalk');
 const url = require('url');
 const path = require('path');
 
-async function createWindow(filePath, {width, height, minWidth, minHeight, maxWidth, maxHeight, center, closable, title, icon, parent}){
+async function createWindow(filePath, {width, height, minWidth, minHeight, maxWidth, maxHeight, center, closable, title, icon, parent}, appOptions){
 
     try{
+        let debug = appOptions.debugMode
+        if(debug == true){
+            logger('debug', `Trying to create a new ${chalk.red.bold('BrowserWindow')}.`);
+        }
+
         const window = new BrowserWindow({
             width: width,
             height: height,
+            minWidth: minWidth,
+            minHeight: minHeight,
+            maxWidth: maxWidth,
+            maxHeight: maxHeight,
+            center: center,
+            closable: closable,
+            title: title,
+            icon: icon,
+            parent: parent,
             show: false,
             webPreferences: {
                 nodeIntegration: true
             }
         });
     
+        if(debug == true){
+            logger('debug', `Trying to load a file type url. Path: ${chalk.yellow.bold(filePath)}`);
+        }
+
         window.loadURL(url.format({
             pathname: path.join(__dirname, filePath),
             protocol: 'file',
             slashes: true,
         }));
 
+        if(debug == true){
+            logger('debug', `Trying to show "${chalk.blue.bold('window')}"`);
+        }
+
         window.once('ready-to-show', () => {
             window.show();
         });
 
+        if(debug == true){
+            logger('debug', `A new BrowserWindow was successfully created. Path: ${chalk.green.bold(filePath)}`);
+        }
         return window;
 
     }catch(e){
-        //logger('debug', '');
-        //logger('error', '');
+        logger('error', `An error occurred trying to create a new BrowserWindow. ${chalk.red.bold(e)}`);
     }
 
 }
