529041ca4a4ecc4844572771fdeac5dd22cf8d91
Klout <55015851+KloutDevs@users.noreply.github.com>
2020-12-21 23:56:45 -0300

    Updated code
    1. The variable mainWindow, no longer exists, instead, use "window", which is obtained in the then of createWindow.
    2. The path was corrected.
    3. Window menu removed.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app } = require('electron');
 const { createWindow } = require('../Nedram/utils/BrowserWindow.js');
+const { logger } = require('./utils/logger.js');
 const appOptions = require('./appOptions.json');
 const url = require('url');
 const path = require('path');
@@ -10,8 +11,9 @@ if(process.env.NODE_ENV !== 'production'){
     });
 }
 
-let mainWindow;
 
-app.on('ready', () => {
-    mainWindow = createWindow('views/index.html', {width: 800, height: 600}, appOptions);
+app.on('ready', async () => {
+    await createWindow(path.join(__dirname, 'views/index.html'), {width: 800, height: 600, show: true}, appOptions).then(window => {
+        window.setMenu(null);
+    });
 });
