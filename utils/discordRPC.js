6aa628589d28c6b62a6ae0cb3793c677207a04e6
Klout <55015851+KloutDevs@users.noreply.github.com>
2021-01-13 14:33:25 -0300

    Update discordRPC.js

--- utils/discordRPC.js
+++ utils/discordRPC.js
@@ -1,27 +1,44 @@
-exports.connect = () => {
+let appOptions = require('../appOptions.json');
+const DiscordRPC = require('discord-rpc');
+const chalk = require('chalk');
+const {logger} = require('./logger.js');
+const clientId = '797352093064232960';
+DiscordRPC.register(clientId);
+const rpc = new DiscordRPC.Client({transport: 'ipc'});
+let timelapse = new Date().getTime();
 
-    const DiscordRPC = require('discord-rpc');
-    const chalk = require('chalk');
-    const {logger} = require('./logger.js');
-    const clientId = '797352093064232960';
-    DiscordRPC.register(clientId);
-    const rpc = new DiscordRPC.Client({transport: 'ipc'});
-    let timestamp;
-    
-    function initPresence() {
-      rpc.setActivity({
-        details: `Developing app...`,
-        startTimestamp: timestamp,
-        instance: false
-      });
-    }
+function initPresence() {
+  rpc.setActivity({
+    details: `idle`,
+    state: `Beta ${appOptions.appVersion}`,
+    startTimestamp: timelapse,
+    largeImageKey: 'nedramicon',
+    largeImageText: 'https://github.com/KloutDevs/Nedram/',
+    instance: false
+  });
+}
 
-    rpc.on('ready', () => {
-      initPresence();
-    });
+rpc.on('ready', () => {
+  initPresence();
+});
 
-    rpc.login({clientId}).catch(e => {
+let connectPromise = new Promise(async (resolve, reject) => {
+    rpc.login({clientId}).then((Client) => {
+        resolve(Client);
+    }).catch(e => {
         logger('error', `An error occurred with the DiscordRPC function, the error is as follows: ${chalk.red.bold(e)}`);
+        resolve(e.message);
     });
+});
 
+let getClient = new Promise(async (resolve, reject) => {
+    await connectPromise.then(async (Client, type) => {
+        resolve(Client);
+    });
+});
+
+
+module.exports = {
+    connectPromise,
+    getClient
 }
