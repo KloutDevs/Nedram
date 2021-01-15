23c117c3a9630b3106e09c5997862dee7a92d314
Klout <55015851+KloutDevs@users.noreply.github.com>
2021-01-15 10:09:58 -0300

    Create welcomeFile.js

---
+++ public/js/welcomeFile.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.recentFiles-title').classList.add('animate__animated');
+    document.querySelector('.recentFiles-title').classList.add('animate__backInDown');
+    document.querySelector('.app-info-img').classList.add('animate__animated');
+    document.querySelector('.app-info-img').classList.add('animate__backInRight');
+});
