13886a2d5f5e7d5af8522b5f934adf3c6b5c0331
Klout <55015851+KloutDevs@users.noreply.github.com>
2021-01-28 18:39:30 -0300

    Updated JS: welcomeFile btn's click
    Now all buttons have a function onclick

--- public/js/welcomeFile.js
+++ public/js/welcomeFile.js
@@ -1,6 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.recentFiles-title').classList.add('animate__animated');
-    document.querySelector('.recentFiles-title').classList.add('animate__backInDown');
-    document.querySelector('.app-info-img').classList.add('animate__animated');
-    document.querySelector('.app-info-img').classList.add('animate__backInRight');
-});
+function colorThemeBtn(){
+    /* OPEN SETTING AND COLOR THEME SECTION */
+}
+function programShortcutBtn(){
+    /* OPEN SETTING AND PROGRAM SHORTCUTS SECTION */
+}
+function programLanguageBtn(){
+    /* OPEN SETTING AND PROGRAM LANGUAGE SECTION */
+}
+function howToDiagramBtn(){
+    /* OPEN BROWSER AND GO TO OFFICIAL WEBPAGE IN THE HOW TO DIAGRAM SECTION */
+}
+function howToGraphicBtn(){
+    /* OPEN BROWSER AND GO TO OFFICIAL WEBPAGE IN THE HOW TO GRAPHIC SECTION */
+}
+function howToExportBtn(){
+    /* OPEN BROWSER AND GO TO OFFICIAL WEBPAGE IN THE HOW TO EXPORT SECTION */
+}
+function newFileBtn(){
+    openNewFileWindow();
+}
+function openFileBtn(){
+    /* openFile() */
+}
+function openFolderBtn(){
+    /* openFolder() */
+}
