630ce83c3feca19183967dea50c9767880189d71
Klout <55015851+KloutDevs@users.noreply.github.com>
2021-01-28 18:40:40 -0300

    Updated JS: Notifications in Titlebar
    Added new notifications in titlebar (With clear all notifications button).

--- public/js/titlebar.js
+++ public/js/titlebar.js
@@ -11,7 +11,61 @@ document.querySelector('#btn-minimize').addEventListener('click', () => {
         remote.getCurrentWindow().minimize();
     }
 });
-
+document.querySelector('#btn-notifications').addEventListener('click', () => {
+    document.querySelector('#notifications_box').classList.toggle('notifications_box_show');
+});
+document.querySelector('.clearNotificationsBtn').addEventListener('click', () => {
+    let notificationsBody = document.querySelector('.notifications_body');
+    if(notificationsBody.childNodes.length > 0){
+        for(let x=0;x < notificationsBody.childNodes.length;x++){
+            if(notificationsBody.childNodes[0].innerText == 'No new notifications have arrived here...'){
+                return;
+            }else{
+                notificationsBody.childNodes[x].classList.add('animate__animated');
+                notificationsBody.childNodes[x].classList.add('animate__zoomOutUp');
+            }
+        }
+        setTimeout(function(){
+            while(notificationsBody.childNodes.length >= 1){
+                notificationsBody.removeChild(notificationsBody.childNodes[0]);
+            }
+            let withoutNotification = document.createElement('span');
+            withoutNotification.innerHTML = 'No new notifications have arrived here...';
+            withoutNotification.classList.add('notification_withoutNotifications');
+            notificationsBody.insertBefore(withoutNotification, null);
+        }, 900);
+    }else{
+        return;
+    }
+});
+ipcRenderer.on('newNotification', (event, body) => {
+    let color, ___;
+    if(body.type == 'error'){
+        ___ = 'Error in';
+        color = '#ff5050';
+    }else if(body.type == 'alert'){
+        ___ = 'Alert in';
+        color = '#ffff66';
+    }else if(body.type == 'info'){
+        ___ = 'Info from';
+        color = '#66ff66';
+    }else{
+        return;
+    }
+    let notification_item = document.createElement('div');
+    notification_item.classList.add('notification_item');
+    notification_item.innerHTML = `                   <div class="notification_item_icon">
+                        <i class="fal fa-exclamation-circle notification_icon"></i> 
+                    </div>
+                    <div class="notification_item_text">
+                        <p class="notification_text">${___} module <b>${body.module}</b>: ${body.message}</p>
+                    </div>`;
+    notification_item.childNodes[1].style.color = color;
+    if(document.querySelector('.notifications_body').childNodes.length == 1 && document.querySelector('.notifications_body').childNodes[0].innerText == "No new notifications have arrived here..." || document.querySelector('.notifications_body').childNodes[0].nodeName == "#text"){
+        document.querySelector('.notifications_body').removeChild(document.querySelector('.notifications_body').childNodes[0]);
+    }
+    document.querySelector('.notifications_body').insertBefore(notification_item, null);
+});
 document.querySelector('#btn-maximize').addEventListener('click', () => {
     if(remote.getCurrentWindow().isMaximized() == true){
         remote.getCurrentWindow().unmaximize();
